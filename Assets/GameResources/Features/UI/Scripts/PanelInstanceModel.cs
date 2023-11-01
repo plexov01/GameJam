10148447cf212bfc51ff140519e5d11e8adb04b2
plexov01 <plexov.01@mail.ru>
2023-11-01 19:40:56 +0700

    Реализована подгрузка панелей, кнопка показывающая панель

---
+++ Assets/GameResources/Features/UI/Scripts/PanelInstanceModel.cs
@@ -0,0 +1,22 @@
+﻿namespace GameJam.Features.UI
+{ 
+	using UnityEngine;
+	using System;
+	using UnityEngine.Serialization;
+
+	/// <summary>
+	/// Экземпляр панели
+	/// </summary>
+	[Serializable]
+	public class PanelInstanceModel
+	{
+		/// <summary>
+		/// Id панели
+		/// </summary>
+		public string PanelId = default;
+		/// <summary>
+		/// Экзмепляр панели
+		/// </summary>
+		public GameObject Panel = default;
+	}
+}
---
+++ Assets/GameResources/Features/UI/Scripts/PanelPool.cs
@@ -0,0 +1,64 @@
+namespace GameJob.Features.UI
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Unity.VisualScripting;
+    using UnityEngine;
+    
+    /// <summary>
+    /// Пулл панелей
+    /// </summary>
+    public class PanelPool : MonoBehaviour
+    {
+        private List<GameObject> _prebasPanelForPool = new List<GameObject>();
+        private List<GameObject> _pooledPanels = new List<GameObject>();
+
+        private void Awake()
+        {
+            _prebasPanelForPool = Resources.LoadAll<GameObject>("PrefabsPanel").ToList();
+            foreach (Transform child in transform)
+            {
+                _pooledPanels.Add(child.gameObject);
+            }
+        }
+        /// <summary>
+        /// Получить панель из пула
+        /// </summary>
+        /// <param name="panelName"></param>
+        public GameObject GetPanelFromPool(string panelName)
+        {
+            GameObject instance = _pooledPanels.FirstOrDefault(x => x.name == panelName);
+
+            if (instance!=null)
+            {
+                instance.SetActive(true);
+                return instance;
+            }
+
+            GameObject prefab = _prebasPanelForPool.FirstOrDefault(x => x.name == panelName);
+            if (prefab != null)
+            {
+                // Create a new instance
+                GameObject newInstance = Instantiate(prefab, Vector3.zero, Quaternion.identity, transform);
+         
+                // Make sure you set it's name (so you remove the Clone that Unity ads)
+                newInstance.name = panelName;
+
+                // Set it's position to zero
+                newInstance.transform.localPosition = Vector3.zero;
+         
+                _pooledPanels.Add(newInstance);
+         
+                return newInstance;
+            }
+      
+            Debug.LogWarning("Object pool doesn't have a prefab for the object with name " + panelName);
+            return null;
+            
+            
+        }
+
+
+    }
+}
+
