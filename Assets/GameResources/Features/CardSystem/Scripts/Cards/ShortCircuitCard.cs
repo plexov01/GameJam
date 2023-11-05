ca46608a0c8cb686ee1e472bb29bfe50ce3d76db
plexov01 <plexov.01@mail.ru>
2023-11-06 04:29:29 +0700

    Карточка с коротким замыканием

--- Assets/GameResources/Features/CardSystem/Scripts/Cards/ShortCircuitCard.cs
+++ Assets/GameResources/Features/CardSystem/Scripts/Cards/ShortCircuitCard.cs
@@ -1,13 +1,65 @@
 ﻿namespace GameJam.Features.CardSystem
 {
+	using System.Collections.Generic;
+	using System.Linq;
 	using UnityEngine;
+	
 	[CreateAssetMenu(fileName = "ShortCircuitCard", menuName = "Cards/ShortCircuitCard")]
 	public class ShortCircuitCard :AbstractCard
 	{
 		public override void ActivateCard()
 		{
 			base.ActivateCard();
-			Debug.Log("ShortCircuitCard");
+
+			float coolness = CoolnessScaleController.Instance.GetCoolness();
+			
+			List<GameObject> walls = GameObject.FindGameObjectsWithTag("Wall").ToList();
+			for (int i = 0; i < walls.Count; i++)
+			{
+				if (Random.Range(0f,1f) > coolness)
+				{
+					Destroy(walls[i]);
+				}	
+			}
+			
+			List<GameObject> turrets = GameObject.FindGameObjectsWithTag("Turret").ToList();
+			for (int i = 0; i < turrets.Count; i++)
+			{
+				if (Random.Range(0f,1f) > coolness)
+				{
+					Destroy(turrets[i]);
+				}	
+			}
+                            			
+			List<GameObject> bomb = GameObject.FindGameObjectsWithTag("Bomb").ToList();
+			for (int i = 0; i < bomb.Count; i++)
+			{
+				if (Random.Range(0f,1f) > coolness)
+				{
+					Destroy(bomb[i]);
+				}	
+			}
+			
+			//main base не уничтожается
+			// List<GameObject> mainBase = GameObject.FindGameObjectsWithTag("MainBase").ToList();
+			// for (int i = 0; i < walls.Count; i++)
+			// {
+			// 	if (Random.Range(0f,1f) > coolness)
+			// 	{
+			// 		Destroy(mainBase[i]);
+			// 	}	
+			// }
+			
+			List<GameObject> rats = GameObject.FindGameObjectsWithTag("Enemy").ToList();
+			
+			for (int i = 0; i < rats.Count; i++)
+			{
+				if (Random.Range(0f,1f) < coolness)
+				{
+					Destroy(rats[i]);
+				}	
+			}
+
 		}
 	}
 }
