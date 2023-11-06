a697515f06ed5349a796e2579af548bf85d092de
Stepan Lutoshkin <62564571+S1lentSt0rm@users.noreply.github.com>
2023-11-06 17:56:43 +0300

    Card params update

--- Assets/GameResources/Features/Tower Defense/Scripts/Mine.cs
+++ Assets/GameResources/Features/Tower Defense/Scripts/Mine.cs
@@ -19,6 +19,8 @@ public class Mine : MonoBehaviour
                     enemy.GetComponent<IDamageable>().TakeDamage(damage);
                 }
             }
+            
+            CoolnessScaleController.Instance.AddCoolness(40);
 
             Destroy(gameObject);
         }
